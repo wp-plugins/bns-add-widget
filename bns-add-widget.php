f25225553e1f689629dcd3acd06c6390203f97ed
cais <cais@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-11 18:37:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/bns-add-widget/tags/0.1.1@204304 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bns-add-widget.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: BNS Add Widget
+Plugin URI: http://buynowshop.com/plugins/bns-add-widget
+Description: Add a widget area to the footer of any theme.
+Version: 0.1.1
+Author: Edward Caissie
+Author URI: http://edwardcaissie.com/
+*/
+
+/*
+**
+* Copyright 2010 Edward Caissie 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* Plugin Changelog: see readme.txt
+**
+*/
+
+global $wp_version;
+$exit_message = 'BNS Add Widget requires WordPress version 2.9 or newer. <a href="http://codex.wordpress.org/Upgrading_WordPress">Please Update!</a>';
+if (version_compare($wp_version, "2.9", "<")) {
+	exit ($exit_message);
+}
+
+/* Hook BNS Widget into 'init' */
+add_action( 'init', 'add_BNS_Add_Widget_Code' );
+function add_BNS_Add_Widget_Code() {
+  if ( function_exists('register_sidebar') ) {
+  register_sidebar(array(
+      'name' => 'BNS Add Widget',
+      'id' => 'bns-add-widget',
+      'before_widget' => '
+        <div class="bns-add-widget">
+          <div class="bns-widget-before"></div>
+          <div class="bns-widget-content">',
+          
+      'after_widget' => '
+          </div><!-- .bns-widget-content -->
+          <div class="bns-widget-after"></div>
+        </div><!-- .bns-add-widget -->',
+      'before_title' => '<h2 class="bns-widget-title">',
+      'after_title' => '</h2>',
+      ));
+  }
+}
+
+/* Hook BNS Widget into wp_footer */
+add_action('wp_footer', 'add_BNS_Widget_to_Footer');
+function add_BNS_Widget_to_Footer() {
+  if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 'bns-add-widget' ) ) : else : ?>
+    <span align="center">You are using the <a href="http://buynowshop.com/plugins/bns-add-widget/">BNS Add Widget</a> plugin! Thank You!</span>
+  <?php endif;
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== BNS Add Widget ===
+Contributors: cais
+Donate link: http://buynowshop.com
+Tags: widget, footer, admin
+Requires at least: 2.9
+Tested up to: 2.9.1
+Stable tag: 0.1.1
+
+Add a widget area to the footer of any theme.
+
+== Description ==
+
+Add a widget area to the footer of any theme. Works just like the widget areas commonly created with code in the functions.php template file.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `bns-add-widget.php` to the `/wp-content/plugins/` directory
+2. Activate through the 'Plugins' menu.
+3. Read http://wpfirstaid.com/2009/12/plugin-installation/
+4. Add your widget choice to the BNS Add Widget area under Appearance | Widgets
+
+-- or -
+
+1. Go to 'Plugins' menu under your Dashboard
+2. Click on the 'Add New' link
+3. Search for bns-add-widget
+4. Install.
+5. Activate through the 'Plugins' menu.
+6. Read http://wpfirstaid.com/2009/12/plugin-installation/
+7. Add your widget choice to the BNS Add Widget area under Appearance | Widgets
+
+== Frequently Asked Questions ==
+
+= How can I style the plugin output? =
+
+The plugin uses the register_sidebar() WordPress function; additional classes are added via the plugin code:
+
+* bns-add-widget
+* bns-widget-before
+* h2.bns-widget-title
+* bns-widget-content
+* bns-widget-after
+
+== Screenshots ==
+
+1. The new blank widget area
+
+== Other Notes ==
+* Copyright 2009, 2010 Edward Caissie 
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+== Upgrade Notice ==
+* Initial release.
+
+== Changelog ==
+= 0.1.1 =
+* clarified the plugin's release under a GPL license
+
+= 0.1 =
+* Initial release.
